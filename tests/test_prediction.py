2d4a8dc8da7088cb9180321b590c7cbf41392cff
paulmuschiol <paul.henry.muschiol@pwc.com>
2021-04-03 10:43:49 +0000

    reworked tests

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,19 +1,27 @@
-import requests
 import numpy as np
+from app import app
+import pytest
 
 PORT = '5000'
 
+@pytest.fixture(scope="module")
+def client():
+    app.config['TESTING'] = True
+    return app.test_client()
 
-def test_prediction():
-    r = requests.post('http://localhost:{}/predict'.format(PORT), json={
+def test_prediction(client):
+    json_payload = {
         'CHAS': {'0': 0},
         'RM': {'0': 6.575},
         'TAX': {'0': 296.0},
         'PTRATIO': {'0': 15.3},
         'B': {'0': 396.9},
         'LSTAT': {'0': 4.98},
-        })
-    prediction = r.json()['prediction'][0]
+        }
+    r = client.post('predict', json=json_payload)
+    #r = requests.post('http://localhost:{}/predict'.format(PORT), json=)
+    #print(r['prediction'][0])
+    prediction = r.get_json()['prediction'][0]
     print(prediction)
     np.testing.assert_almost_equal(prediction, 20.353731, decimal=3)
 
